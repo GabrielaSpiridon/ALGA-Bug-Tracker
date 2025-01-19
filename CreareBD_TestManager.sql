ccee4e0bb08c96ca0277ff7e4c94aab909931cee
GabrielaSpiridon <148895361+GabrielaSpiridon@users.noreply.github.com>
2025-01-19 20:18:03 +0200

    CreareBD_TestManager.sql

--- CreareBD_TestManager.sql
+++ CreareBD_TestManager.sql
@@ -1,77 +1,105 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS TEST_MANAGER_DB;
+
 -- Create the database
-CREATE DATABASE IF NOT EXISTS TEST_MANAGER_DB;
+CREATE DATABASE TEST_MANAGER_DB;
 USE TEST_MANAGER_DB;
 
--- 1. Create BUGURI Table
-CREATE TABLE IF NOT EXISTS BUGURI (
-    id_bug INT(5) AUTO_INCREMENT PRIMARY KEY,
-    nivel_severitate VARCHAR(30),
-    prioritate_rezolvare VARCHAR(30),
-    descriere_bug VARCHAR(300),
-    status_rezolvare VARCHAR(30),
-    id_commit_raportare_bug INT(5),
-    id_commit_rezolvare_bug INT(5),
-    FOREIGN KEY (id_commit_raportare_bug) REFERENCES COMMITURI(id_commit) ON DELETE SET NULL,
-    FOREIGN KEY (id_commit_rezolvare_bug) REFERENCES COMMITURI(id_commit) ON DELETE SET NULL
+-- Drop tables if they exist
+DROP TABLE IF EXISTS PROJECT;
+DROP TABLE IF EXISTS COMMIT;
+DROP TABLE IF EXISTS BUG;
+DROP TABLE IF EXISTS USER;
+DROP TABLE IF EXISTS USER_ROLE;
+DROP TABLE IF EXISTS PROJECT_COMMIT;
+DROP TABLE IF EXISTS PROJECT_USER;
+DROP TABLE IF EXISTS PROJECT_BUG;
+
+-- Create PROJECT TABLE
+CREATE TABLE IF NOT EXISTS PROJECT (
+    id_project INT(5) AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(30),
+    repository_link VARCHAR(150)
 );
 
--- 2. Create PROIECT_BUGURI Table
-CREATE TABLE IF NOT EXISTS PROIECT_BUGURI (
-    id_proiect_bug INT(5) AUTO_INCREMENT PRIMARY KEY,
-    id_proiect INT(5),
-    id_bug INT(5),
-    id_student_raportor INT(5),
-    id_student_rezolvator INT(5),
-    FOREIGN KEY (id_proiect) REFERENCES PROIECT(id_proiect) ON DELETE CASCADE,
-    FOREIGN KEY (id_bug) REFERENCES BUGURI(id_bug) ON DELETE CASCADE,
-    FOREIGN KEY (id_student_raportor) REFERENCES STUDENT(id_student) ON DELETE CASCADE,
-    FOREIGN KEY (id_student_rezolvator) REFERENCES STUDENT(id_student) ON DELETE CASCADE
-);
-
--- 3. Create STUDENT Table
-CREATE TABLE IF NOT EXISTS STUDENT (
-    id_student INT(5) AUTO_INCREMENT PRIMARY KEY,
-    nume_student VARCHAR(255),
-    email_student VARCHAR(255) UNIQUE
-);
-
--- 4. Create ROL_STUDENT Table
-CREATE TABLE IF NOT EXISTS ROL_STUDENT (
-    id_rol INT(5) AUTO_INCREMENT PRIMARY KEY,
-    denumire_rol VARCHAR(30)
-);
-
--- 5. Create PROIECT Table
-CREATE TABLE IF NOT EXISTS PROIECT (
-    id_proiect INT(5) AUTO_INCREMENT PRIMARY KEY,
-    nume_proiect VARCHAR(30),
-    link_repository VARCHAR(150)
-);
-
--- 6. Create COMMITURI Table
-CREATE TABLE IF NOT EXISTS COMMITURI (
+-- Create COMMIT Table
+CREATE TABLE IF NOT EXISTS COMMIT (
     id_commit INT(5) AUTO_INCREMENT PRIMARY KEY,
-    link_commit VARCHAR(150)
+    commit_link VARCHAR(150)
 );
 
--- 7. Create PROIECT-COMMIT Table
-CREATE TABLE IF NOT EXISTS PROIECT_COMMIT (
-    id_proiect_commit INT(5) AUTO_INCREMENT PRIMARY KEY,
-    id_proiect INT(5),
+-- Create BUG Table
+CREATE TABLE IF NOT EXISTS BUG (
+    id_bug INT(5) AUTO_INCREMENT PRIMARY KEY,
+    severity_level VARCHAR(30),
+    solve_priority VARCHAR(30),
+    bug_description VARCHAR(300),
+    solution_status VARCHAR(30),
+    id_commit_report_bug INT(5),
+    id_commit_resolve_bug INT(5),
+    FOREIGN KEY (id_commit_report_bug) REFERENCES COMMIT(id_commit) ON DELETE SET NULL,
+    FOREIGN KEY (id_commit_resolve_bug) REFERENCES COMMIT(id_commit) ON DELETE SET NULL
+);
+
+-- Create USER Table
+CREATE TABLE IF NOT EXISTS USER (
+    id_user INT(5) AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255) UNIQUE,
+    user_password VARCHAR(255)
+);
+
+-- Create USER_ROLE Table
+CREATE TABLE IF NOT EXISTS USER_ROLE (
+    id_role INT(5) AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(30)
+);
+
+-- Create PROJECT_COMMIT Table
+CREATE TABLE IF NOT EXISTS PROJECT_COMMIT (
+    id_project_commit INT(5) AUTO_INCREMENT PRIMARY KEY,
+    id_project INT(5),
     id_commit INT(5),
-    FOREIGN KEY (id_proiect) REFERENCES PROIECT(id_proiect) ON DELETE CASCADE,
-    FOREIGN KEY (id_commit) REFERENCES COMMITURI(id_commit) ON DELETE CASCADE
+    FOREIGN KEY (id_project) REFERENCES PROJECT(id_project) ON DELETE CASCADE,
+    FOREIGN KEY (id_commit) REFERENCES COMMIT(id_commit) ON DELETE CASCADE
 );
 
--- 8. Create PROIECT-STUDENT Table
-CREATE TABLE IF NOT EXISTS PROIECT_STUDENT (
-    id_proiect_student INT(5) AUTO_INCREMENT PRIMARY KEY,
-    id_proiect INT(5),
-    id_student INT(5),
-    id_rol_student INT(5),
-    FOREIGN KEY (id_proiect) REFERENCES PROIECT(id_proiect) ON DELETE CASCADE,
-    FOREIGN KEY (id_student) REFERENCES STUDENT(id_student) ON DELETE CASCADE,
-    FOREIGN KEY (id_rol_student) REFERENCES ROL_STUDENT(id_rol) ON DELETE CASCADE
+-- Create PROJECT_USER Table
+CREATE TABLE IF NOT EXISTS PROJECT_USER (
+    id_project_user INT(5) AUTO_INCREMENT PRIMARY KEY,
+    id_project INT(5),
+    id_user INT(5),
+    id_role_user INT(5),
+    FOREIGN KEY (id_project) REFERENCES PROJECT(id_project) ON DELETE CASCADE,
+    FOREIGN KEY (id_user) REFERENCES USER(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_role_user) REFERENCES USER_ROLE(id_role) ON DELETE CASCADE
 );
+
+-- Create PROJECT_BUG Table
+CREATE TABLE IF NOT EXISTS PROJECT_BUG (
+    id_project_bug INT(5) AUTO_INCREMENT PRIMARY KEY,
+    id_project INT(5),
+    id_bug INT(5),
+    id_user_reporter INT(5),
+    id_user_solver INT(5),
+    FOREIGN KEY (id_project) REFERENCES PROJECT(id_project) ON DELETE CASCADE,
+    FOREIGN KEY (id_bug) REFERENCES BUG(id_bug) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_reporter) REFERENCES USER(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_solver) REFERENCES USER(id_user) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
